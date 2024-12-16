6454ad7dc21763ff85c2f4e596ce2c5dad25ddb8
sathiyapriyancj <ssathiya.cs@gmail.com>
2024-12-16 10:12:13 +0530

    Refactor

---
+++ 3-express-mongoose/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { request } = require('http');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const { dirname } = require('path');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((request, response, next) => {
+  console.log('Hello from middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log('Manipulate request');
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
